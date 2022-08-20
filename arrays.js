94ff9a3bf04b10cbba5d7d4ec16d4a9ccde83993
thenns <samm77@gmail.com>
2022-08-20 16:27:49 -0600

    finished array problems

--- arrays.js
+++ arrays.js
@@ -4,11 +4,13 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+const colorCopy = faveColors.slice();
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push('blue')
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,7 +21,9 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+const removeFirst = numbers.shift();
+const removeLast = numbers.pop();
+const middleNumbs = numbers;
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +38,14 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+const answers = [];
+
+for (let i = 0; i < bigOrSmallArray.length; i++){
+    if (bigOrSmallArray[i] > 100){
+        answers.push('big');
+    }
+    else if (bigOrSmallArray[i] <= 100)
+    {
+        answers.push('small');
+    }   
+}
--- sdlc.js
+++ sdlc.js
@@ -12,10 +12,18 @@
 // Testing & Integration
 // Planning
 
+//Analysis of Requirements
+//Planning
+//Design
+//Testing & Integration
+//Implementation
+//Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
-    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    Create a variable for each of the above steps of the Software Development Lifecycle
+    and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const 
