6280fbf6274b8eab43e413312089a2a550203831
thenns <samm77@gmail.com>
2022-08-24 22:15:19 -0600

    finished the sdlc portion

--- sdlc.js
+++ sdlc.js
@@ -12,11 +12,11 @@
 // Testing & Integration
 // Planning
 
-//Analysis of Requirements
 //Planning
+//Analysis of Requirements
 //Design
-//Testing & Integration
 //Implementation
+//Testing & Integration
 //Maintain
 
 //////////////////PROBLEM 2////////////////////
@@ -25,5 +25,20 @@
     and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
-const 
+const planning = "This is the first step, in which the end goal of the software"
+               + " is determined and defined. Developers typically will see what"
+               + " systems are already in place, and determine what features will need to be added.";
+
+const analysisOfReq = "This is the step where you identify the problems and needs for the projct";
+
+const design = "In this step the plan is reviewed by team members and they offer suggestions and other"
+             + " things that may need to be changed. Guideless and best practices are established here";;
+
+const implementation = "The coding starts in this step. Coders need to stick to the plan and "
+                     + "best practices that were agreed on. Developers will try to get a 
+                     + "working product as soon as possible";
+
+const testingAndIntegration = "In this step, the product is tested and debugged.";
+
+const maintain = "Maintenance is an on going step. New features are implemented, bugs are fixed, security measures 
+               + "are updated, etc";
